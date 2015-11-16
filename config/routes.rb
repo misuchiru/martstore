ee947a71ae9730d4dda7bfaaed7c4e813ace1edf
misuchiru <perryhaha@gmail.com>
2015-11-16 18:28:44 +0800

    完成customer

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   namespace :admin do
     resources :products
+    resources :customers
     resources :users do
       post :to_admin
       post :to_normal
